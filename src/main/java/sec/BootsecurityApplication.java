a8dff17216d1eaa4cb6ba7e8cf490b5b0ad6bddc
vintagecodes <rraj02668@gmail.com>
2024-04-21 15:55:03 +0530

    Updated authentication

--- src/main/java/sec/BootsecurityApplication.java
+++ src/main/java/sec/BootsecurityApplication.java
@@ -2,12 +2,12 @@ package sec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableMongoRepositories
+@EnableDiscoveryClient
 public class BootsecurityApplication {
 
 	public static void main(String[] args) {
