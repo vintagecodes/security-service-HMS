967f87c7b4f7a77365dacb9ef84b12b8db913dbb
vintagecodes <rraj02668@gmail.com>
2024-04-22 02:48:41 +0530

    updated

--- src/main/java/sec/BootsecurityApplication.java
+++ src/main/java/sec/BootsecurityApplication.java
@@ -2,12 +2,10 @@ package sec;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class BootsecurityApplication {
 
 	public static void main(String[] args) {
