8e301b3d379ad570c03f58b70a3e11dceeec0a85
vintagecodes <rraj02668@gmail.com>
2024-04-22 18:17:08 +0530

    updated

--- src/main/java/sec/WebConfig.java
+++ src/main/java/sec/WebConfig.java
@@ -5,9 +5,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class WebConfig {
 	
 	@Bean
@@ -20,7 +22,8 @@ public class WebConfig {
 				corsRegistry.addMapping("/api/user-auth/**")
 				.allowedOrigins("https://thehealthconsultor.netlify.app","http://localhost:4200")
 				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
-				.allowedHeaders(HttpHeaders.CONTENT_TYPE, HttpHeaders.AUTHORIZATION);
+				.allowedHeaders("*")
+				.allowCredentials(true);
 			}
 		};
 	}
