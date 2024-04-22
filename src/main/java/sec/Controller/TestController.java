5483832fbf3a00b171738946f790058f4dbfc5ec
vintagecodes <rraj02668@gmail.com>
2024-04-22 19:17:59 +0530

    updates-I

--- src/main/java/sec/Controller/TestController.java
+++ src/main/java/sec/Controller/TestController.java
@@ -5,19 +5,17 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import sec.model.User;
 import sec.model.service.AuthService;
-//@CrossOrigin(origins = "https://thehealthconsultor.netlify.app", maxAge = 7200, allowCredentials = "true")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 @RestController
 @RequestMapping("/api/user-auth")
 public class TestController {
--- src/main/java/sec/WebConfig.java
+++ src/main/java/sec/WebConfig.java
@@ -1,31 +1,31 @@
-package sec;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-@EnableWebMvc
-public class WebConfig {
-	
-	@Bean
-	public WebMvcConfigurer configurer()
-	{
-		return new WebMvcConfigurer() {
-			
-			public void addCorsMapping(CorsRegistry corsRegistry)
-			{
-				corsRegistry.addMapping("/api/user-auth/**")
-				.allowedOrigins("https://thehealthconsultor.netlify.app","http://localhost:4200")
-				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
-				.allowedHeaders("*")
-				.allowCredentials(true);
-			}
-		};
-	}
-
-}
+//package sec;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpHeaders;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//@EnableWebMvc
+//public class WebConfig {
+//	
+//	@Bean
+//	public WebMvcConfigurer configurer()
+//	{
+//		return new WebMvcConfigurer() {
+//			
+//			public void addCorsMapping(CorsRegistry corsRegistry)
+//			{
+//				corsRegistry.addMapping("/api/user-auth/**")
+//				.allowedOrigins("https://thehealthconsultor.netlify.app","http://localhost:4200")
+//				.allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.OPTIONS.name(), HttpMethod.DELETE.name())
+//				.allowedHeaders("*")
+//				.allowCredentials(true);
+//			}
+//		};
+//	}
+//
+//}
